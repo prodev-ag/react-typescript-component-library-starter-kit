a1612ee06411d46ae04f716cce5de7e79e1452d3
ag <prodev.ag@gmail.com>
2023-02-01 02:58:09 +0400

    check import of icons package into core package

--- packages/core/src/Button/Buttons.tsx
+++ packages/core/src/Button/Buttons.tsx
@@ -1,11 +1,15 @@
 import React from "react";
+import { Add } from '@library/icons';
 
 export interface ButtonProps {
   label: string;
 }
 
 const Button = (props: ButtonProps) => {
-  return <button>{props.label}</button>;
+  return <button>
+    {props.label}
+    <Add />
+  </button>;
 };
 
 export default Button;
